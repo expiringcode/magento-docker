f74f8e1a29e6ff028af70e055ec885dd1585ca26
Ani Sinanaj <ani.sinanaj@caffeina.com>
2019-07-28 22:31:49 +0200

    [+] Leaving command free

--- bin/dev.sh
+++ bin/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose -f ./yml/docker-compose.yml -f ./yml/docker-compose.dev.yml -f ./yml/docker-compose.build.yml up "$@"
+docker-compose -f ./yml/docker-compose.yml -f ./yml/docker-compose.dev.yml -f ./yml/docker-compose.build.yml "$@"
